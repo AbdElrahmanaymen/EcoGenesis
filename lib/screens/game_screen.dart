de87bba917d025d829a8f2d4abecccd7b54b4191
Abd-Elrahman Aymen <Abdelrahmanaymen@outlook.com>
2024-02-14 02:29:44 +0800

    Remove Camera Component

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
 import 'dart:ui';
 
-import 'package:ecogenesis/component/cam_component.dart';
 import 'package:ecogenesis/component/map_component.dart';
 import 'package:ecogenesis/system/map_system.dart';
-import 'package:flame/game.dart';
-import 'package:flame_oxygen/flame_oxygen.dart';
-import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flame/components.dart' show Vector2;
+import 'package:flame_oxygen/flame_oxygen.dart' show OxygenGame;
+import 'package:flame_tiled/flame_tiled.dart' show TiledComponent;
 
 class MyGame extends OxygenGame {
   @override
@@ -15,7 +14,6 @@ class MyGame extends OxygenGame {
   @override
   Future<void> init() async {
     world.registerComponent(MapComponent.new);
-    world.registerComponent(CamComponent.new);
     world.registerSystem(MapSystem());
 
     createEntity(
@@ -30,26 +28,4 @@ class MyGame extends OxygenGame {
       ),
     );
   }
-
-  // late final CameraComponent cam;
-
-  // @override
-  // final world = Map();
-
-  // @override
-  // Future<void> onLoad() async {
-  //   cam = CameraComponent.withFixedResolution(
-  //     width: 3280,
-  //     height: 1900,
-  //     world: world,
-  //   );
-
-  //   cam.viewfinder.anchor = Anchor.topLeft;
-  //   cam.viewfinder.zoom = 1;
-
-  //   addAll([
-  //     world,
-  //     cam,
-  //   ]);
-  // }
 }
